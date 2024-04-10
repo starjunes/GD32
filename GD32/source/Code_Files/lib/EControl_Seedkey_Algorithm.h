e2d2123253b2e2772b01c12a70af12ff47ba3e11
zhangjinzhen <zhangjinzhen@yaxon.com>
2024-04-10 11:54:10 +0800

    Modify:锡柴库头文件优化，防止重定义

--- GD32/source/Code_Files/lib/EControl_Seedkey_Algorithm.h
+++ GD32/source/Code_Files/lib/EControl_Seedkey_Algorithm.h
@@ -1,4 +1,5 @@
-typedef unsigned int DWORD;
-typedef unsigned char uchar;
+//typedef unsigned int DWORD;
+//typedef unsigned char uchar;
+#include "xichai_ems_seedkey.h"
 DWORD seedToKey_EControl_app(uchar *seed,DWORD length,uchar *key,DWORD*retlen);
 DWORD seedToKey_EControl_ota(uchar *seed, DWORD length, uchar *key, DWORD *retLen);
